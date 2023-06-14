9b6028db53811e398a9120313e49331223e60b7d
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-06-14 14:22:59 +0530

    Added codes for create read and delete tweets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import { connect } from "./config/database.js";
 import Tweet from "./models/tweet.js";
+import Hashtag from "./models/hashtag.js";
+import TweetRepository from "./repository/tweet-repository.js";
 const app = express();
 
 
@@ -13,39 +15,24 @@ app.listen(3000, async () => {
     console.log("Mongo DB connected");
 
 
-    Tweet.create({
-        content: "This is my first tweet",
-        likes: 25,
-        noOfRetweets: 5,
-        comment: "This is my firts comment"
-    })
+    // Tweet.create({
+    //     content: "This is my second tweet",
+    //     likes: 27,
+    //     noOfRetweets: 9,
+    //     comment: "This is my second comment"
+    // })
+    // Hashtag.create({
+    //     text: "travel",
+    //     tweets: ['64873e05e6a9eec5f165596e']
+    // })
 
-
-
-
-
-
+    const tweetRepo = new TweetRepository();
+    // let tweets = await tweetRepo.getAllTweets();
+    // let tweet = await tweetRepo.getTweet('648979c45b652ba64319bf9b');
+    let deltweet = await tweetRepo.deleteTweet({
+        _id: '648979c45b652ba64319bf9b'
+    });
+    // console.log('Twwet is: ', tweet);
+    console.log( deltweet);
 })
 
-
-// import express from "express"
-// import {connect} from "./config/database.js";
-// import Tweet from "./models/tweet.js";
-// const app = express();
-
-
-
-// app.listen(3000, async () => {
-//     console.log("server Started at 3000");
-//     // mongo db connection establishment
-
-//     connect();
-//     console.log("Mongo DB connected");
-
-//     Tweet.create({
-//         content: "This is my first tweet",
-//         likes: 25,
-//         noOfRetweets: 5,
-//         comment: "This is my firts comment"
-//     })
-// })
